3923c4863060bb4891d891346d8762bb46e0ef45
Dastan <dastisch123@gmail.com>
2026-09-17 14:21:24 +0300

    fix CORS origin to use https instead of http

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ app.add_middleware(
     allow_origins=[
     "http://127.0.0.1:5173",
     "http://localhost:5173",
-    "http://frontend-dastan.toolforge.rest",
+    "https://frontend-dastan.toolforge.rest",
 ],
     allow_credentials=True,
     allow_methods=["*"],
